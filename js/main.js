79a6c25c08f98599b7cd7b86a2db4db5477cf51a
Michael <michael.owen6@homeoffice.gsi.gov.uk>
2018-10-13 10:42:03 +0100

    dev: update js to smoother scrolling

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
-window.smoothScroll = function(target) {
+var smoothScroll = function(elementId) {
+    var MIN_PIXELS_PER_STEP = 16;
+    var MAX_SCROLL_STEPS = 30;
+    var target = document.getElementById('second');
     var scrollContainer = target;
-    do { //find scroll container
+    do {
         scrollContainer = scrollContainer.parentNode;
         if (!scrollContainer) return;
         scrollContainer.scrollTop += 1;
     } while (scrollContainer.scrollTop == 0);
 
     var targetY = 0;
-    do { //find the top of target relatively to the container
+    do {
         if (target == scrollContainer) break;
         targetY += target.offsetTop;
     } while (target = target.offsetParent);
 
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 20);
-    }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+    var pixelsPerStep = Math.max(MIN_PIXELS_PER_STEP,
+                                 (targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS);
+
+    var stepFunc = function() {
+        scrollContainer.scrollTop =
+            Math.min(targetY, pixelsPerStep + scrollContainer.scrollTop);
+
+        if (scrollContainer.scrollTop >= targetY) {
+            return;
+        }
+
+        window.requestAnimationFrame(stepFunc);
+    };
+
+    window.requestAnimationFrame(stepFunc);
 }
